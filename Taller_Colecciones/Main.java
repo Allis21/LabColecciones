52134f4c677fa5c3d98c2f7ec6f3667f63dae5a6
Joseph Yulian Garcia <josephy.garciac@uqvirtual.edu.co>
2024-08-26 22:55:25 -0500

    Punto 4, 7, 8
    ...

--- Taller_Colecciones/Main.java
+++ Taller_Colecciones/Main.java
@@ -12,7 +12,7 @@ public class Main{
         Map<String, Estudiante> mapEstudiantes = new HashMap<>();
 
 
-        Persona persona1 = new Persona("Juan", 40, "M", "123");
+        Persona persona1 = new Persona("Juan", 16, "M", "123");
         Persona persona2 = new Persona("Maria", 35, "F", "456");
         Persona persona3 = new Persona("Pedro", 36, "M", "789");
 
@@ -31,11 +31,10 @@ public class Main{
         String matriculaBuscada = "A125";
         Estudiante estudianteBuscado = mapEstudiantes.get(matriculaBuscada);
 
-        // punto 1
+        // PUNTO 1
         Persona.setCriterio(criterio);
         Collections.sort(personas);
         System.out.println("Ordenado por edad:  \n" + personas);
-
         Persona.setCriterio(criterio2);
         Collections.sort(personas);
         System.out.println("Ordenado por Nombre:  \n" + personas);
@@ -53,7 +52,7 @@ public class Main{
         System.out.println("El producto con codigo " + codigoBuscar + " es " + empresa.buscarProducto(empresa.getProductos(), codigoBuscar).getNombre());
 
 
-        // punto 3
+        // PUNTO 3
         if(estudianteBuscado != null){
             System.out.println("Estudiante encontrado: " + estudianteBuscado + " con matrícula " + matriculaBuscada);
         } else {
@@ -71,12 +70,72 @@ public class Main{
         elementos.clear();
         elementos.addAll(eliminaDuplicados);
 
-        // punto 5
+        // PUNTO 4
+        Stack<Object> stack = new Stack<>();
+        stack.add(estudiante1);
+        stack.add(empresa);
+        stack.add(producto1);
+
+        agregarAPila(stack, estudiante1);
+
+        // PUNTO 5
         ListIterator<String> listIterator = elementos.listIterator();
         while(listIterator.hasNext()) {
             String next = listIterator.next();
             System.out.println("[ " + next + " ]");
         }
+
+        // PUNTO 7
+        List<Persona> listaFiltrada = new ArrayList<>();
+        List<Persona> personasMayoresEdad = listMayoresEdad(personas, listaFiltrada, 0);
+        System.out.println("Personas Mayores de Edad: "+ personasMayoresEdad);
+
+        // PUNTO 8
+        Map<Integer, String> mapa = new HashMap<>();
+        mapa.put(1, "MERA");
+        mapa.put(2, "GGGG");
+        mapa.put(3, "FFFF");
+        mapa.put(4, "AYFKM");
+
+        Map<Integer, String> mapaPar = new HashMap<>();
+        Iterator<Map.Entry<Integer, String>> iterator = mapa.entrySet().iterator();
+        Map<Integer, String> mapaFiltrado = mapClavesPares(mapa, mapaPar, iterator);
+        System.out.println("Mapa con Key Par: "+ mapaFiltrado);
+    }
+
+    public static void agregarAPila(Stack<Object> stack, Object objeto){
+        if(stack.peek().getClass().equals(objeto.getClass())){
+            stack.add(objeto);
+        }
+        else{
+            System.out.println("No se puede añadir el objeto");
+        }
+    }
+
+    public static List<Persona> listMayoresEdad(List<Persona> personas, List<Persona> listaFiltrada, int i){
+        if(i==personas.size()) {
+            return listaFiltrada;
+        }
+
+        Persona persona = personas.get(i);
+        if(persona.getEdad()>18){
+            listaFiltrada.add(persona);
+        }
+        return listMayoresEdad(personas, listaFiltrada, i+1);
+    }
+
+    public static Map<Integer, String> mapClavesPares(Map<Integer, String> mapa, Map<Integer, String> mapaPar, Iterator<Map.Entry<Integer, String>> iterator){
+        if(!iterator.hasNext()){
+            return mapaPar;
+        }
+
+        Map.Entry<Integer, String> siguiente = iterator.next();
+
+        if(siguiente.getKey() % 2 == 0){
+            mapaPar.put(siguiente.getKey(), siguiente.getValue());
+        }
+
+        return mapClavesPares(mapa, mapaPar, iterator);
     }
 }
 
